f73c74e51aeda1651c1809b1326d4f938ce6949b
Arnav6508 <arnavgupta6508@gmail.com>
2025-01-12 20:11:45 +0530

    added test_accuracy function to calculate accuracy for unseen data

--- inference.py
+++ inference.py
@@ -1,20 +1,12 @@
 import pickle
 import numpy as np
 import tensorflow as tf
-from utils import create_model
+from load import load_weights
+from utils import compute_accuracy
 
 def predict(sentence):
 
-    with open('model_weights/side_weights.pkl', 'rb') as file:
-        side_weights = pickle.load(file)
-    tag_map = side_weights['tag_map']
-    sentence_vectorizer = side_weights['sentence_vectorizer']
-    vocab = side_weights['vocab']
-
-    ## B4 loading weights model needs to be built so it knows input dim
-    model = create_model(len(tag_map), len(vocab), embedding_dim = 50)
-    model.build(input_shape=(None, len(vocab)+1)) 
-    model.load_weights('model_weights/model_weights.weights.h5')
+    tag_map, sentence_vectorizer, model = load_weights()
 
     vectorized_sentence = sentence_vectorizer(sentence)
     vectorized_sentence = tf.expand_dims(vectorized_sentence, axis = 0) ## add batch dim
@@ -33,3 +25,8 @@ def predict(sentence):
         print(word,':',ner_label)
 
     return pred
+
+def test_accuracy(test_sentences, test_labels):
+    tag_map, sentence_vectorizer, model = load_weights()
+    acc = compute_accuracy(model, test_sentences, test_labels, sentence_vectorizer, tag_map)
+    return acc
